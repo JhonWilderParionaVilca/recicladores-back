251c55bb62664b74cab4dda80a733831815bfebb
JhonWilderParionaVilca <jhon.pariona@smartpyme.pe>
2022-05-25 13:01:35 -0500

    fix(deploy): change port railway

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,5 +1,5 @@
 export const APP = {
-  PORT_SERVER: process.env.PORT_SERVER || "4000",
+  PORT_SERVER: process.env.PORT || "4000",
   JWT_AUTH_SECRET: process.env.JWT_AUTH_SECRET,
   CLOUDINARY_CLOUD_NAME: `${process.env.CLOUD_NAME}`,
   CLOUDINARY_API_KEY: `${process.env.API_KEY}`,
