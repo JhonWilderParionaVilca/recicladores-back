698c7ca84a61a4803bbb50805ce348569d6c9644
JHON WILDER PARIONA VILCA <jhon.pariona.27@unsch.edu.pe>
2022-05-27 16:57:06 -0500

    Update nearCollectors.validator.ts

--- src/Collectors/validators/nearCollectors.validator.ts
+++ src/Collectors/validators/nearCollectors.validator.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const queryRequestNearCollectorYup = yup.object({
-  items: yup.string().lowercase().trim().required("ingrese un item"),
+  items: yup.string().required("ingrese un item"),
   longitude: yup
     .number()
     .min(-199, "valor minimo longitud -199")
